78c06b75224e856c80755bd8ef4b29c0ed9aa7aa
Crispin <crispin@developster.io>
2016-06-30 11:52:22 +1200

    woo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rpg-sim
+
+### Credits
+Uses [Toen's Medieval Strategy Sprite Pack](http://opengameart.org/content/toens-medieval-strategy-sprite-pack-v10-16x16)under CC-A.40
--- game.js
+++ game.js
@@ -1,10 +1,21 @@
 /**
  * Created by crispin on 3/06/16.
  */
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'div');
+var game = new Phaser.Game(800, 800, Phaser.AUTO, 'div');
+var goodGuys = [];
+var scale = 7;
+//takes grid [x, y] returns game [x, y]
+var getGridCoords  = function(x, y){
+	console.log(map.widthInPixels * scale)
+};
 
 var loadState = {
 	preload: function () {
+		game.stage.smoothed = false;
+		game.antialias = false;
+		game.load.tilemap('arena', 'assets/battlemap.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.image('tileset', 'assets/tileset.png');
+		game.load.image('archer', 'assets/archer.png');
 			},
 	create: function () {
 		game.state.start('play');
@@ -13,10 +24,34 @@ var loadState = {
 
 var playState = {
 	create: function () {
+		map = game.add.tilemap('arena');
+		map.addTilesetImage('Toen', 'tileset');
+		bg = map.createLayer('Background');
+		fg = map.createLayer('Foreground');
+		person = game.add.sprite(0, 0, 'archer');
+		person.inputEnabled = true;
+
+		bg.scale = {x:scale, y:scale};
+		fg.scale = {x:scale, y:scale};
+
+		person.scale = {x: scale, y: scale};
+
+		bg.fixedToCamera = false;
+
+		bg.x = this.world.centerX - (map.widthInPixels * scale / 2);
+		bg.y = this.world.centerY - (map.heightInPixels * scale / 2);
+
+		fg.fixedToCamera = false;
+
+		fg.x = this.world.centerX - (map.widthInPixels * scale / 2);
+		fg.y = this.world.centerY - (map.heightInPixels * scale / 2);
+
+		person.events.onInputDown.add(function () {getGridCoords(1,1)}, this);
 
 	},
 	update: function () {
-
+		person.x = game.input.mousePointer.x - person.width / 2;
+		person.y = game.input.mousePointer.y - person.height / 2;
 
 	}
 };
