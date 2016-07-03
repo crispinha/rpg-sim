9eddb5769e83218e62f088e7265904116e79ee2b
Crispin <crispin@developster.io>
2016-07-04 09:32:17 +1200

    Added a test background and snapping to grid.

--- game.js
+++ game.js
@@ -6,18 +6,21 @@ var goodGuys = [];
 var scale = 7;
 //takes grid [x, y] returns game [x, y]
 var getGridCoords  = function(x, y){
-	console.log(map.widthInPixels * scale)
+	if (x > map.width || y > map.width) {
+		throw RangeError("x or y is too big");
+	}
+	return [bg.x + (map.tileWidth * scale * (x - 1)), bg.y + (map.tileHeight * scale * (y - 1))];
 };
-
 var loadState = {
 	preload: function () {
 		game.stage.smoothed = false;
 		game.antialias = false;
-		game.load.tilemap('arena', 'assets/battlemap.json', null, Phaser.Tilemap.TILED_JSON);
-		game.load.image('tileset', 'assets/tileset.png');
+		game.load.tilemap('arena', 'assets/holodeck.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.image('tileset', 'assets/holodeck.png');
 		game.load.image('archer', 'assets/archer.png');
 			},
 	create: function () {
+		game.stage.backgroundColor = "#4488AA";
 		game.state.start('play');
 	}
 };
@@ -25,14 +28,14 @@ var loadState = {
 var playState = {
 	create: function () {
 		map = game.add.tilemap('arena');
-		map.addTilesetImage('Toen', 'tileset');
+		map.addTilesetImage('holodeck', 'tileset');
 		bg = map.createLayer('Background');
-		fg = map.createLayer('Foreground');
+		// fg = map.createLayer('Foreground');
 		person = game.add.sprite(0, 0, 'archer');
 		person.inputEnabled = true;
 
 		bg.scale = {x:scale, y:scale};
-		fg.scale = {x:scale, y:scale};
+		// fg.scale = {x:scale, y:scale};
 
 		person.scale = {x: scale, y: scale};
 
@@ -41,17 +44,16 @@ var playState = {
 		bg.x = this.world.centerX - (map.widthInPixels * scale / 2);
 		bg.y = this.world.centerY - (map.heightInPixels * scale / 2);
 
-		fg.fixedToCamera = false;
-
-		fg.x = this.world.centerX - (map.widthInPixels * scale / 2);
-		fg.y = this.world.centerY - (map.heightInPixels * scale / 2);
-
-		person.events.onInputDown.add(function () {getGridCoords(1,1)}, this);
-
+		// fg.fixedToCamera = false;
+		//
+		// fg.x = this.world.centerX - (map.widthInPixels * scale / 2);
+		// fg.y = this.world.centerY - (map.heightInPixels * scale / 2);
+		[person.x,person.y] = getGridCoords(1, 1);
+		person.events.onInputDown.add(function () {[person.x,person.y] = getGridCoords(6, 6);}, this);
 	},
 	update: function () {
-		person.x = game.input.mousePointer.x - person.width / 2;
-		person.y = game.input.mousePointer.y - person.height / 2;
+		// person.x = game.input.mousePointer.x - person.width / 2;
+		// person.y = game.input.mousePointer.y - person.height / 2;
 
 	}
 };
